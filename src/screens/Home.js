739996781cf4dbf82b0494b11732c9383da9856f
Carlos <carlos.martinez@obsidiansoft.io>
2019-12-11 12:36:20 -0600

    added image

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,41 @@
 import React from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
+
+// Utils
+import colors from "../utils/colors";
 
 class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.container}>
-          <Text style={styles.title}>Bienvenido</Text>
-        </View>
-        <View style={styles.container}></View>
-        <View style={styles.container}></View>
-        <View style={styles.container}></View>
+        <ImageBackground
+          source={require("../assets/backgroundImage.jpeg")}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <View style={[styles.container, styles.centerContents]}>
+            <Text style={styles.title}>Bienvenido</Text>
+          </View>
+          <View style={[styles.container, styles.centerContents]}>
+            <Image
+              source={{
+                uri: "https://www.titecnoeducacion.com.mx/images/imgs/Logo.png"
+              }}
+              style={{
+                width: 150,
+                height: 60,
+              }}
+            />
+          </View>
+          <View style={styles.container}></View>
+          <View style={styles.container}></View>
+        </ImageBackground>
       </View>
     );
   }
