de27f346ccea083d81e2c168ee586825236bc00d
Carlos <carlos.martinez@obsidiansoft.io>
2019-12-12 14:31:01 -0600

    added Geolocalizacion

--- src/screens/Geolocalizacion.js
+++ src/screens/Geolocalizacion.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from "react-native";
 
 // Expo
 import * as Permissions from "expo-permissions";
@@ -7,8 +13,24 @@ import * as Location from "expo-location";
 
 // Styles
 import customStyles from "../utils/styles";
+import colors from "../utils/colors";
+
+function RenderLocation({ latitude, longitude, onPress }) {
+  return (
+    <View>
+      <Text style={styles.text}>
+        latitude: {latitude}
+        {"\n"}longitude: {longitude}
+      </Text>
+      <TouchableHighlight onPress={onPress}>
+        <Text style={styles.text}>Obtener Dirección</Text>
+      </TouchableHighlight>
+    </View>
+  );
+}
 
 function Geolocalizacion() {
+  const [loading, setLoading] = useState(true);
   const [location, setLocation] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
 
@@ -20,22 +42,64 @@ function Geolocalizacion() {
       } else {
         const location = await Location.getCurrentPositionAsync({});
         setLocation(location);
+        console.log(JSON.stringify(location));
       }
+      setLoading(false);
     }
+    getLocationAsync();
   }, []);
+
+  /**
+   * {"coords":
+   *  {
+   *    "altitude":0,
+   *    "altitudeAccuracy":-1,
+   *    "latitude":37.785834,
+   *    "accuracy":5,
+   *    "longitude":-122.406417,
+   *    "heading":-1,
+   *    "speed":-1
+   * },
+   * "timestamp":1576178996909.2932}
+   */
+  const { coords } = location || {};
+  const { latitude, longitude } = coords || {};
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Geolocalización</Text>
-      <View style={styles.container}></View>
+      {loading && (
+        <View style={[styles.container, styles.centerContents]}>
+          <ActivityIndicator size='large' color={colors.black} />
+        </View>
+      )}
+      {!loading && (
+        <View style={styles.container}>
+          {errorMessage ? (
+            <Text>{errorMessage}</Text>
+          ) : (
+            <View>
+              <Text style={styles.text}>
+                latitude: {latitude}
+                {"\n"}longitude: {longitude}
+              </Text>
+              <TouchableHighlight>
+                <Text style={styles.text}>Obtener Dirección</Text>
+              </TouchableHighlight>
+            </View>
+          )}
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   ...customStyles,
-  containerB: {
-    flex: 3
+  text: {
+    textAlign: "center",
+    fontSize: 24,
+    marginVertical: 10
   }
 });
 
