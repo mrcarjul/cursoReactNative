1314539f4236eae26493e977aba70c1e622d29fb
Carlos <carlos.martinez@obsidiansoft.io>
2019-12-11 19:20:34 -0600

    added useEffect to manage push notifications

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+// Core
 import { StyleSheet, View } from "react-native";
+
+// Components
 import Navigation from "./src/navigation";
 import Loading from "./src/components/Loading";
 
+// Expo
+import { Notifications } from "expo";
+
 export default function App() {
   const [visible, setVisible] = useState(false);
+  const [notification, setNotification] = useState([]);
+
+  useEffect(() => {
+    const notificationSubscription = Notifications.addListener(
+      handleNotification
+    );
+    debugger;
+  }, []);
+
+  const handleNotification = not => {
+    console.log(JSON.stringify(not));
+    /* {
+      data: {
+        visible: true,
+        value: 1
+      },
+      ...
+    } */
+    setNotification(not);
+  };
+
   return (
     <View style={styles.container}>
       <Navigation screenProps={{ visible, setVisible }} />
