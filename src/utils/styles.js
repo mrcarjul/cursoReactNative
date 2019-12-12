56ce17e968cfb99e205efd226db012b2774a72d8
Carlos <carlos.martinez@obsidiansoft.io>
2019-12-12 13:26:17 -0600

    added generic styles

---
+++ src/utils/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+});
