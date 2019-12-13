d93cd6b8d697b5355172ef76739f3a3b101450e1
Carlos <carlos.martinez@obsidiansoft.io>
2019-12-13 14:45:21 -0600

    added bottomTabNavigator

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -6,7 +6,8 @@ import { Dimensions } from "react-native";
 import {
   createAppContainer,
   createDrawerNavigator,
-  createSwitchNavigator
+  createSwitchNavigator,
+  createBottomTabNavigator
 } from "react-navigation";
 
 // Screens
@@ -23,27 +24,73 @@ import Practica4 from "../practicas/practica4";
 import Practica5 from "../practicas/practica5";
 
 import colors from "../utils/colors";
+import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const { width } = Dimensions.get("window");
 
-const Drawer = createDrawerNavigator(
+const TabNavigator = createBottomTabNavigator(
   {
-    Home: { screen: Home },
     Practica1: { screen: Practica1 },
     Practica2: { screen: Practica2 },
     Practica3: { screen: Practica3 },
     Practica4: { screen: Practica4 },
+    Practica5: { screen: Practica5 }
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        const IconComponent = MaterialCommunityIcons;
+        let iconName;
+        if (routeName === "Practica1") {
+          iconName = `animation`;
+        } else if (routeName === "Practica2") {
+          iconName = `animation-outline`;
+        } else if (routeName === "Practica3") {
+          iconName = `animation-play`;
+        } else if (routeName === "Practica4") {
+          iconName = `animation-play-outline`;
+        } else if (routeName === "Practica5") {
+          iconName = `adjust`;
+        }
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray"
+    }
+  }
+);
+
+const Drawer = createDrawerNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        title: "Dashboard",
+        drawerIcon: (
+          <MaterialIcons name='dashboard' size={21} color={colors.white} />
+        )
+      }
+    },
     Geolocalizacion: { screen: Geolocalizacion },
     Gallery: { screen: Gallery },
-    Practica5: { screen: Practica5 }
+    Practicas: { screen: TabNavigator }
   },
   {
     initialRouteName: "Home",
     contentOptions: {
-      activeBackgroundColor: colors.white,
-      activeTintColor: colors.blue
+      activeBackgroundColor: colors.black,
+      activeTintColor: colors.blue,
+      inactiveTintColor: colors.white
     },
-    drawerWidth: width * 0.5
+    drawerBackgroundColor: colors.gray,
+    drawerWidth: width * 0.5,
+    labelStyle: {
+      fontSize: 21,
+      fontWeight: "bold"
+    }
   }
 );
 
